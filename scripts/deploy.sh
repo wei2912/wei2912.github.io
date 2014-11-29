53e2c76ebe0c4c347fe84834631082a7422de5a5
Wei En <wei2912.supp0rt@gmail.com>
2014-11-29 11:44:57 +0800

    Fix deploy problems due to SSH key permissions and Git config.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 echo "* Start of deploy process"
 
 cd _site/
+git config user.email "weien1292@gmail.com"
+git config user.name "Wei En"
+git config --global push.default simple
 git remote set-url origin git@github.com:wei2912/wei2912.github.io
 git commit -a -m "update at $(date)"
 git push
