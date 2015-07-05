8c7ac05c6fcdd5f8f8e20794ac5c3d4475f899a8
Wei En <wei2912.supp0rt@gmail.com>
2015-07-05 22:48:58 +0800

    Change serif font to Lora.

--- src/compilesass.hs
+++ src/compilesass.hs
@@ -3,13 +3,14 @@ module Hakyll.Web.CompileSass
 , scssCompiler
 ) where
 
+import Control.Monad
 import Hakyll
 
 compileWith :: [String] -> Compiler (Item String)
 compileWith flags =
-    getResourceString
+    liftM (fmap compressCss) (getResourceString
         >>= withItemBody (unixFilter "sass" flags)
-        >>= return . fmap compressCss
+    )
 
 sassCompiler :: Compiler (Item String)
 sassCompiler = compileWith ["-s"]
