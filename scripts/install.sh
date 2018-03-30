7ef7c2ab90e07fc0733a3ad9b1d4afe1f9d3e7ea
Ng Wei En <weien1292@gmail.com>
2018-03-30 23:44:58 +0800

    let's try again

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ echo "* Start of build process."
 
 sudo apt-get update
 gem install sass || exit 1
-wget -qO- https://get.haskellstack.org/ | sudo || exit 1
+wget -qO- https://get.haskellstack.org/ | sh || exit 1
 
 echo "* Installed dependencies."
 
