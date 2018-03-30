f3e909276a5a1d7551a0664507bf3d455792578d
Ng Wei En <weien1292@gmail.com>
2018-03-30 23:27:29 +0800

    some other installation stuff

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,6 @@
 echo "* Start of build process."
 
 sudo apt-get update
-sudo apt-get install -y ruby ruby-dev || exit 1
 gem install sass || exit 1
 sudo -c 'curl -sSL https://get.haskellstack.org/ | sh' || exit 1
 
