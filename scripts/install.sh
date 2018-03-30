20bcb289963d3e5aae509304f8f84ecacdeba73a
Ng Wei En <weien1292@gmail.com>
2018-03-30 23:14:39 +0800

    cache just stack packages

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,6 @@ echo "* Start of build process."
 sudo apt-get install rubygems build-essential || exit 1
 curl -sSL https://get.haskellstack.org/ | sh || exit 1
 gem install sass || exit 1
-stack setup || exit 1
-stack install || exit 1
 
 echo "* Installed dependencies."
 
