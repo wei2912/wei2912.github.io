9a911089ed536b5df3b0af6dbd8c7cce1aaadd2d
Ng Wei En <weien1292@gmail.com>
2018-03-30 23:20:09 +0800

    fix slight errors with commands

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,9 +3,9 @@
 echo "* Start of build process."
 
 sudo apt-get update
-sudo apt-get install rubygems build-essential || exit 1
-curl -sSL https://get.haskellstack.org/ | sh || exit 1
+sudo apt-get install ruby ruby-dev || exit 1
 gem install sass || exit 1
+sudo -c 'curl -sSL https://get.haskellstack.org/ | sh' || exit 1
 
 echo "* Installed dependencies."
 
