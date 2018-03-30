7226fd2e999e5f70fa65b037a03b47d32359e591
Ng Wei En <weien1292@gmail.com>
2018-03-30 23:39:03 +0800

    use wget instead of curl

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ echo "* Start of build process."
 
 sudo apt-get update
 gem install sass || exit 1
-sudo 'curl -sSL https://get.haskellstack.org/ | sh' || exit 1
+sudo 'wget -qO- https://get.haskellstack.org/ | sh' || exit 1
 
 echo "* Installed dependencies."
 
