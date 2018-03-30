b6214b4ca0f8102952f177e5d5589cf80e5304ce
Ng Wei En <weien1292@gmail.com>
2018-03-30 23:22:56 +0800

    add y flag to apt-get

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 echo "* Start of build process."
 
 sudo apt-get update
-sudo apt-get install ruby ruby-dev || exit 1
+sudo apt-get install -y ruby ruby-dev || exit 1
 gem install sass || exit 1
 sudo -c 'curl -sSL https://get.haskellstack.org/ | sh' || exit 1
 
