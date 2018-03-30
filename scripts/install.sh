4ae41967c47d26c3f2a7f10364a0e83813f09e70
Ng Wei En <weien1292@gmail.com>
2018-03-30 23:17:04 +0800

    do an apt-get update before installing packages

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,7 @@
 
 echo "* Start of build process."
 
+sudo apt-get update
 sudo apt-get install rubygems build-essential || exit 1
 curl -sSL https://get.haskellstack.org/ | sh || exit 1
 gem install sass || exit 1
