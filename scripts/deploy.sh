b0609b271efb4f8c541056efd958e4b5cc4bfc31
Ng Wei En <weien1292@gmail.com>
2018-03-31 23:58:39 +0800

    Have the deploy script stage and commit *all* changes.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ git config user.email "weien1292@gmail.com"
 git config user.name "Ng Wei En"
 git config --global push.default simple
 git remote set-url origin git@github.com:wei2912/blog || exit 1
-git commit -a -m "update at $(date)" || exit 1
+git add -A && git commit -m "update at $(date)" || exit 1
 git push || exit 1
 cd ..
 
