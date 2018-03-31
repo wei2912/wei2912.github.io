571125cadad9b9deb4d7a8ae9a813896fa540018
Ng Wei En <weien1292@gmail.com>
2018-03-31 14:02:14 +0800

    remove unknown cmd

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ echo "* Start of install process."
 
 mkdir -p ~/.local/bin
 export PATH=$HOME/.local/bin:$PATH
-travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 
 echo "** Installed Stack."
 
