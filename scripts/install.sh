0bd3c87b1f0bdb8ea85e148bee95b447af49308c
Ng Wei En <weien1292@gmail.com>
2018-09-08 01:11:43 +0800

    Revert previous commit [ci skip]

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,11 +4,11 @@ echo "* Start of install process."
 
 mkdir -p ~/.local/bin
 export PATH=$HOME/.local/bin:$PATH
-travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 
 echo "** Installed Stack."
 
-travis_retry gem install sass
+gem install sass
 
 echo "** Installed SASS."
 
