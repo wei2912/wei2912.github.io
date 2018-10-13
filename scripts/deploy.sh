b50508f3e6f3f7ce28348232f7c0f0384049273a
Ng Wei En <weien1292@gmail.com>
2018-10-13 19:31:34 +0800

    Remove unnecessary log messages from deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,8 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ];
 then
-  echo "* Skipping deploy for pull request"
   exit 0
 fi
-
-echo "* Start of deploy process"
 
 cd _site/
 git config user.email "weien1292@gmail.com"
@@ -16,5 +13,3 @@ git remote set-url origin git@github.com:wei2912/blog || exit 1
 git add -A && git diff-index --quiet HEAD || git commit -m "update at $(date)" || exit 1
 git push || exit 1
 cd ..
-
-echo "* Deployed site."
