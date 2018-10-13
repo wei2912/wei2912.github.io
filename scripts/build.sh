e493e52a168d8a7946193c116665a0eb4c7f6c95
Ng Wei En <weien1292@gmail.com>
2018-10-13 19:32:42 +0800

    Remove unnecessary log messages from build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-echo "* Start of build process."
-
-stack --no-terminal --skip-ghc-check build
-
-echo "* Compiled binaries."
+stack --no-terminal --skip-ghc-check build || exit 1
 
 git clone https://github.com/wei2912/blog _site/ || exit 1
 stack exec blog-src build || exit 1
-
-echo "* Built site."
-ls -R _site/
