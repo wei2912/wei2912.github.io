6403a50e128ce0dc8b4b0ab074eec51867f51e66
Ng Wei En <weien1292@gmail.com>
2018-10-13 19:08:55 +0800

    Replace the Ruby gem sass with dart-sass
    This replaces the deprecated Ruby gem, as explained in https://github.com/wei2912/blog-src/issues/12.

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,12 +4,13 @@ echo "* Start of install process."
 
 mkdir -p ~/.local/bin
 export PATH=$HOME/.local/bin:$PATH
-curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+
+curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 '*/stack'
 
 echo "** Installed Stack."
 
-gem install sass
+curl -L https://github.com/sass/dart-sass/releases/download/1.14.2/dart-sass-1.14.2-linux-x64.tar.gz | tar xz --wildcards --strip-components=1 '*/sass'
 
-echo "** Installed SASS."
+echo "** Installed dart-sass."
 
 echo "* Installed all dependencies."
