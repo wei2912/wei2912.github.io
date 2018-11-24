ea5b591f5588f63ce21cde179544d3821dec0586
Ng Wei En <weien1292@gmail.com>
2018-11-25 00:46:39 +0800

    remove Ruby gem

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,8 +8,8 @@ curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip
 
 echo "** Installed Stack."
 
-gem install sass
 
-echo "** Installed SASS."
+
+
 
 echo "* Installed all dependencies."
